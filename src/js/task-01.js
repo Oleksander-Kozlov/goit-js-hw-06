66d23df161cfb9e2dbd4e7d1ec334bfc8c2c704e
Oleksander-Kozlov <kozlov.oleksandr89@gmail.com>
2023-03-24 01:33:21 +0200

    Update task-01.js

---
+++ src/js/task-01.js
@@ -0,0 +1,9 @@
+const list = document.querySelector("#categories");
+console.log(`Number of categories: ${list.children.length}`);
+[...list.children].forEach(item => {
+    console.log(`Category: ${item.firstElementChild.textContent}
+    Elements: ${item.lastElementChild.children.length}`);
+    
+});
+
+
