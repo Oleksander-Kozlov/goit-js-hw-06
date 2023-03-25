950d9c0445fc73d42ac9783e4e3d567844e27adb
Oleksander-Kozlov <kozlov.oleksandr89@gmail.com>
2023-03-25 22:05:25 +0200

    Update task-05.js

---
+++ src/js/task-05.js
@@ -0,0 +1,7 @@
+const textInput = document.querySelector("#name-input");
+
+const output = document.querySelector("#name-output");
+console.log(textInput);
+textInput.addEventListener("input", (event) => {
+  output.textContent = event.currentTarget.value;
+});
