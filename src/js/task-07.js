295a862f7155e9d8607828127bc6799f0bc76ab3
Oleksander-Kozlov <kozlov.oleksandr89@gmail.com>
2023-03-26 16:47:25 +0200

    Update task-07.js

---
+++ src/js/task-07.js
@@ -0,0 +1,10 @@
+const fontSizeChanger = document.querySelector("#font-size-control");
+
+const textExemple = fontSizeChanger.nextElementSibling.nextElementSibling;
+
+fontSizeChanger.addEventListener("input", ranger)
+
+function ranger(evt) {
+    textExemple.style.fontSize = `${evt.currentTarget.value}px`
+    console.log(evt.currentTarget.value)
+};
