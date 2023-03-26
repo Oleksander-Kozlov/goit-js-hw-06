36f530876897698925e6e84a51830ef40c4ec0e8
Oleksander-Kozlov <kozlov.oleksandr89@gmail.com>
2023-03-26 21:31:00 +0200

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -6,11 +6,21 @@
 // </div>
 // Для генерування випадкового кольору використовуй функцію getRandomHexColor.
 
+const btnChangeColor = document.querySelector(".change-color");
+const body = document.querySelector('body');
+const whatColor = document.querySelector('.color');
+console.log(whatColor);
 
-
+btnChangeColor.addEventListener("click", changer)
+function changer() {
+  body.style.backgroundColor = getRandomHexColor();
+  whatColor.textContent = `${getRandomHexColor()}`
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+console.log(body);
