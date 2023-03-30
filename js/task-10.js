1e8c223da0b8de16cfca319eba00b4a9039ca289
Oleksander-Kozlov <kozlov.oleksandr89@gmail.com>
2023-03-30 03:50:59 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,31 +1,87 @@
 const optionals = document.querySelector("#controls"); 
 
+const container = document.querySelector("#boxes");
+
+const clear = optionals.lastElementChild;
 const numInput = optionals.firstElementChild;
 const btnCreate = optionals.firstElementChild.nextElementSibling;
-btnCreate.addEventListener("click", create)
-
-function create {
-  
+btnCreate.addEventListener("click", createBoxes);
+let boxes = 0
+numInput.addEventListener("input", quantity)
+function quantity(evt) {
+  return boxes = Number.parseInt(evt.currentTarget.value);
+ }
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
 
-
-
-console.dir(btnCreate);
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
-// 
-//   createBoxes()
-// };
-// function createBoxes(amount)  { 
+function createBoxes() {
+  const items = [];
   
+  for (let i = 0; i < boxes; i++ ) {
+    const color = getRandomHexColor();
+    const item = document.createElement("div");
+    item.classList = "button";
+    item.dataset.color = color;
+    item.style.backgroundColor = color;
+    item.style.width = `${10*i+20}px`;
+    item.style.height = `${10 * i + 20}px`;
+    item.classList.add("item");
+    items.push(item);
+  }
+  container.append(...items);
+}
+
+clear.addEventListener("click", onClick);
+
+function onClick() {
+  container.innerHTML = "";
+}
+// let divTotal = [];
+// let step = 0;
+// function createBoxes(amount) {
+
+// function quantity(evt) {
+//   amount = evt.value;
+//   for (let i = 1; i < amount; i += 1) {
+//     const container = document.createElement("div");
+//     step += 10;
+//     container.style.width = `${20 + step}px`;
+//     container.style.height = `${20 + step}px`;
+//     const currentСolor = getRandomHexColor();
+//     container.style.backgroundColor = currentСolor;
+//     divTotal.push(item);
+//   }
+// }
+
+//   // createBoxes(numBoxes);    
+// }
+
+
+
+// function onClick(evt) { 
+//   for (let i = 1; i<
+// }
+
+
+
+
+
+// 
+
+
+
+
+// function onClick(evt) {
+//   step += 10;
+//   container.style.width = `${20+step}px`;
+//   container.style.height = `${20+step}px`;
+// const currentСolor = getRandomHexColor()
+// container.style.backgroundColor = currentСolor;
 // }
 
 // function destroyBoxes() {
   
 // };
-
-
-
